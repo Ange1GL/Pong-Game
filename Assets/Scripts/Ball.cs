252ecf99208dd1b830fb3161153b6d5797e7b18f
Ange1GL <2021003@utsh.edu.mx>
2024-11-09 11:30:53 -0600

    feat: Agrega la funcionalidad de bola que puede colisionar y chocar con el escenario y los jugadadores, video 2 de 7

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class Scripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 7;
+    public Rigidbody2D rb;
+    public Vector2 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+        Launch();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+
+    public void Reset()
+    {
+        transform.position = startPos;
+        rb.velocity = Vector2.zero;
+        Launch();
+    }
+
+    public void Launch()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+
+        rb.velocity = new Vector2(speed * x, speed * y);
+
+
+
+        
+
+
+    }
+}
